2ecaced49500191ea331040547934416a84cb20a
emrexhepi <em.rexhepi@gmail.com>
2018-09-15 14:36:57 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import React, { Component } from 'react';
+
+// bootstrap 4 react
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
-import React, { Component } from 'react';
+// style
+import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container z-depth-2">
         <h1> CV </h1>
       </div>
     );
